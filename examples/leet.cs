363eaa55d55c95659acddf88f51d45518fb3a0f9
CowNation <CowNation.cow@gmail.com>
2020-01-07 07:28:04 -0600

    Create leet.cs

---
+++ examples/leet.cs
@@ -0,0 +1,22 @@
+using System;
+
+class MainClass
+{
+  public static void Main (string[] args)
+	{
+		CowConfig.WriteConfig writeCfg = new CowConfig.WriteConfig("test.txt");
+		writeCfg.Section("Ints");
+		writeCfg.WriteLine("three: ", 3.ToString());
+		writeCfg.Section("Strings");
+		writeCfg.WriteLine("ell: ", "L");
+		writeCfg.WriteLine("tee: ", "T");
+		writeCfg.Close();
+
+		CowConfig.ReadConfig readCfg = new CowConfig.ReadConfig("test.txt");
+		Console.WriteLine(readCfg.Read<string>("", "myn: "));
+		Console.WriteLine(readCfg.Read<string>("Strings", "ell: "));
+		Console.WriteLine(readCfg.Read<int>("Ints", "three: "));
+		Console.WriteLine(readCfg.Read<int>("Ints", "three: "));
+		Console.WriteLine(readCfg.Read<string>("Strings", "tee: "));
+	}
+}
