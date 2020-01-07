5925eb412417c24365ee8d8dcb929d233be1543e
CowNation <CowNation.cow@gmail.com>
2020-01-07 07:40:49 -0600

    Create leet.cpp

---
+++ examples/leet.cpp
@@ -0,0 +1,15 @@
+#include "cpp-CowConfig.h"
+#include <string>
+
+int main() {
+  CowConfig cfg("test.txt");
+	cfg.Section("Ints");
+	cfg.WriteLine("three: ", "3");
+	cfg.Section("Strings");
+	cfg.WriteLine("ell: ", "L");
+	cfg.WriteLine("tee: ", "T");
+	std::cout << cfg.Read<std::string>("Strings", "ell: ") << std::endl;
+	std::cout << cfg.Read<int>("Ints", "three: ") << std::endl;
+	std::cout << cfg.Read<int>("Ints", "three: ") << std::endl;
+	std::cout << cfg.Read<std::string>("Strings", "tee: ") << std::endl;
+}
